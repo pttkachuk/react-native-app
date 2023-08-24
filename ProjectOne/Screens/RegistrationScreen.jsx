59d4655fd0c86f8e999c539105749179cfe634e2
pttkachuk <petrotkachuk6@gmail.com>
2023-08-24 17:37:04 +0200

    add registrationscreen base markup

--- ProjectOne/Screens/RegistrationScreen.jsx
+++ ProjectOne/Screens/RegistrationScreen.jsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+//import background from "../images/registration-bg.jpg";
 
 const RegistrationScreen = () => {
   return (
     <View>
-      <Text>RegistrationScreen</Text>
+      <ImageBackground>
+        <View>
+          <Text>Реєстрація</Text>
+          <TextInput placeholder="Логін" />
+          <TextInput placeholder="Адреса електронної пошти" />
+          <TextInput placeholder="Пароль" />
+          <TouchableOpacity>
+            <Text>Показати</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Зареєструватися</Text>
+          </TouchableOpacity>
+          <Text>Вже є акаунт? Увійти</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
