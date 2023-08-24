fa73ce752956b04a4678dfb1280e78628f79aabd
pttkachuk <petrotkachuk6@gmail.com>
2023-08-24 15:28:19 +0200

    create postscreen

---
+++ ProjectOne/Screens/PostScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PostScreen = () => {
+  return (
+    <View>
+      <Text>PostScreen</Text>
+    </View>
+  );
+};
+
+export default PostScreen;
