cdbcdf33cb6b5a7c74e4a454de4e13b1b99c0c7b
pttkachuk <petrotkachuk6@gmail.com>
2023-08-24 17:36:52 +0200

    add loginscreen base markup

--- ProjectOne/Screens/LoginScreen.jsx
+++ ProjectOne/Screens/LoginScreen.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <ImageBackground>
+        <View>
+          <Text>Увійти</Text>
+          <TextInput placeholder="Адреса електронної пошти" />
+          <TextInput placeholder="Пароль" />
+          <TouchableOpacity>
+            <Text>Показати</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Увійти</Text>
+          </TouchableOpacity>
+          <Text>Немає акаунту? Зареєструватися</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
