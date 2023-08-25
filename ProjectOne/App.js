7ecb09c4405799ba8be177f9d25aa8a105563901
pttkachuk <petrotkachuk6@gmail.com>
2023-08-25 20:37:27 +0200

    test

--- ProjectOne/App.js
+++ ProjectOne/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './Screens/RegistrationScreen';
-//import LoginScreen from './Screens/LoginScreen';
+import LoginScreen from './Screens/LoginScreen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ 'Roboto': require('./assets/fonts/Roboto-Regular.ttf') });
@@ -11,7 +11,7 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <RegistrationScreen />
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
