850b652bc368524407a4abce3f54fe3e542e7819
pttkachuk <petrotkachuk6@gmail.com>
2023-08-25 10:37:42 +0200

    add styles

--- ProjectOne/Screens/RegistrationScreen.jsx
+++ ProjectOne/Screens/RegistrationScreen.jsx
@@ -1,29 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImageBackground,
+  StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-//import background from "../images/registration-bg.jpg";
+import { AntDesign } from "@expo/vector-icons";
+import background from "../images/registration-bg.jpg";
+
+const initialState = {
+  nickname: "",
+  email: "",
+  password: "",
+};
 
 const RegistrationScreen = () => {
+  const [state, setState] = useState(initialState);
+  const [isShowKeybord, setIsShowKeybord] = useState(false);
+  const [isLoginFocused, setIsLoginFocused] = useState(false);
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+
+  const onChangeLogin = (text) => {
+    setState((prevState) => ({ ...prevState, nickname: text.trim() }));
+  };
+
+  const onChangeEmail = (text) => {
+    setState((prevState) => ({ ...prevState, email: text.trim() }));
+  };
+
+  const onChangePassword = (text) => {
+    setState((prevState) => ({ ...prevState, password: text.trim() }));
+  };
+
   return (
-    <View>
-      <ImageBackground>
-        <View>
-          <Text>Реєстрація</Text>
-          <TextInput placeholder="Логін" />
-          <TextInput placeholder="Адреса електронної пошти" />
-          <TextInput placeholder="Пароль" />
-          <TouchableOpacity>
-            <Text>Показати</Text>
+    <View style={styles.container}>
+      <ImageBackground source={background} style={styles.backgroundImage}>
+        <View style={styles.innerContainer}>
+          <View>
+            <TouchableOpacity>
+              <AntDesign name="plus" size={25} color="#FF6C00" />;
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.title}>Реєстрація</Text>
+          <TextInput
+            style={[styles.input, isLoginFocused && styles.inputFocus]}
+            onFocus={() => {
+              setIsShowKeybord(true), setIsLoginFocused(true);
+              setIsEmailFocused(false);
+              setIsPasswordFocused(false);
+            }}
+            onBlur={() => setIsLoginFocused(false)}
+            placeholder="Логін"
+            value={state.nickname}
+            onChangeText={onChangeLogin}
+          />
+          <TextInput
+            style={[styles.input, isEmailFocused && styles.inputFocus]}
+            onFocus={() => {
+              setIsShowKeybord(true), setIsEmailFocused(true);
+              setIsLoginFocused(false);
+              setIsPasswordFocused(false);
+            }}
+            onBlur={() => setIsEmailFocused(false)}
+            placeholder="Адреса електронної пошти"
+            value={state.email}
+            onChangeText={onChangeEmail}
+          />
+          <TextInput
+            style={[styles.input, isPasswordFocused && styles.inputFocus]}
+            onFocus={() => {
+              setIsShowKeybord(true), setIsPasswordFocused(true);
+              setIsLoginFocused(false);
+              setIsEmailFocused(false);
+            }}
+            onBlur={() => setIsPasswordFocused(false)}
+            placeholder="Пароль"
+            value={state.password}
+            onChangeText={onChangePassword}
+          />
+          <TouchableOpacity style={styles.showPassword}>
+            <Text style={styles.showPasswordText}>Показати</Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Зареєструватися</Text>
+          <TouchableOpacity style={styles.button} activeOpacity={0.5}>
+            <Text style={styles.titlebutton}>Зареєструватися</Text>
           </TouchableOpacity>
-          <Text>Вже є акаунт? Увійти</Text>
+          <Text style={styles.titletext}>Вже є акаунт? Увійти</Text>
         </View>
       </ImageBackground>
     </View>
@@ -31,3 +95,79 @@ const RegistrationScreen = () => {
 };
 
 export default RegistrationScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "flex-end",
+    width: "100%",
+  },
+  innerContainer: {
+    width: "100%",
+    height: 550,
+    alignItems: "center",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: "#fff",
+  },
+  title: {
+    color: "#212121",
+    textAlign: "center",
+    fontSize: 30,
+    fontFamily: "Roboto",
+    fontWeight: 500,
+    lineHeight: 35,
+    letterSpacing: 0.3,
+    marginTop: 32,
+    marginBottom: 32,
+  },
+  input: {
+    width: 343,
+    height: 50,
+    margin: 8,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: "#F6F6F6",
+  },
+  inputFocus: {
+    borderColor: "#FF6C00",
+    borderWidth: 1,
+  },
+  showPassword: {
+    top: -40,
+    left: 130,
+  },
+  showPasswordText: {
+    color: "#1B4371",
+    fontFamily: "Roboto",
+    fontWeight: 400,
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    height: 50,
+    width: 343,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+    marginTop: 20,
+  },
+  titlebutton: {
+    color: "#FFF",
+    fontFamily: "Roboto",
+    fontSize: 16,
+    fontWeight: 400,
+  },
+  titletext: {
+    color: "#1B4371",
+    fontFamily: "Roboto",
+    fontSize: 16,
+    fontWeight: 400,
+    textAlign: "center",
+    marginTop: 16,
+  },
+});
