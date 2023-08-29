fa732046512c1c4abe1bb6f3b5261941901341c8
pttkachuk <petrotkachuk6@gmail.com>
2023-08-29 10:25:23 +0200

    add main navigation

--- ProjectOne/App.js
+++ ProjectOne/App.js
@@ -1,8 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+//////////////////////////////////////////////
 import RegistrationScreen from "./Screens/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen";
+import Home from "./Screens/Home";
+import CommentsScreen from "./Screens/CommentsScreen";
+import CreatePostsScreen from "./Screens/CreatePostsScreen";
+import MapScreen from "./Screens/MapScreen";
+import PostScreen from "./Screens/PostScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+//////////////////////////////////////////////
+
+const MainStack = createStackNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,16 +25,28 @@ export default function App() {
     return null;
   }
   return (
-    <View style={styles.container}>
-      <RegistrationScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainStack.Navigator initialRouteName="RegisterScreen">
+        <MainStack.Screen
+          name="Registration"
+          options={{ headerShown: false }}
+          component={RegistrationScreen}
+        />
+        <MainStack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={LoginScreen}
+        />
+        <MainStack.Screen name="Home" component={Home} />
+        <MainStack.Screen
+          name="CreatePostsScreen"
+          component={CreatePostsScreen}
+        />
+        <MainStack.Screen name="PostScreen" component={PostScreen} />
+        <MainStack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <MainStack.Screen name="MapScreen" component={MapScreen} />
+        <MainStack.Screen name="CommentsScreen" component={CommentsScreen} />
+      </MainStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
