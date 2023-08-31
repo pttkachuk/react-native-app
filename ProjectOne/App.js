75e04bc0cf6004debeb66c881a7e89237d0e29b4
pttkachuk <petrotkachuk6@gmail.com>
2023-08-31 10:06:54 +0200

    add new font

--- ProjectOne/App.js
+++ ProjectOne/App.js
@@ -21,6 +21,7 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
   });
   if (!fontsLoaded) {
     return null;
