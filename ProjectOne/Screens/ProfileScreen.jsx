4a19018f74a1a726d059f233d130ce037b4ab1ba
pttkachuk <petrotkachuk6@gmail.com>
2023-09-01 16:10:13 +0200

    createProfile screen

--- ProjectOne/Screens/ProfileScreen.jsx
+++ ProjectOne/Screens/ProfileScreen.jsx
@@ -1,19 +1,94 @@
 import React from "react";
-import { Text, View } from "react-native";
+import background from "../images/registration-bg.jpg";
+import Avatar from "../images/profile-img.jpg";
+import { Feather } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const ProfileScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
+    <ImageBackground
+      resizeMode="cover"
+      source={background}
+      style={styles.bgImage}
     >
-      <Text>ProfileScreen</Text>
-    </View>
+      <View style={styles.wrapper}>
+        <TouchableOpacity style={{ position: "absolute", right: 16, top: 22 }}>
+          <Feather name="log-out" size={24} color="#BDBDBD" />
+        </TouchableOpacity>
+        <View style={styles.photoContainer}>
+          <Image style={styles.avatar} source={Avatar} />
+          <TouchableOpacity style={styles.deletePhotoButton}>
+            <Octicons name="plus-circle" size={23} color="#FF6C00" />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.text}>Name Surname</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
 export default ProfileScreen;
+const styles = StyleSheet.create({
+  wrapper: {
+    alignContent: "flex-end",
+    height: "80%",
+    width: "100%",
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: "white",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+  },
+  photoContainer: {
+    position: "absolute",
+    top: -60,
+    left: "50%",
+    transform: [{ translateX: -45 }],
+    width: 120,
+    height: 120,
+    backgroundColor: "#F6F6F6",
+    borderRadius: 15,
+  },
+  deletePhotoButton: {
+    position: "absolute",
+    top: 81,
+    right: -12.5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 25,
+    height: 25,
+    borderColor: "#E8E8E8",
+    backgroundColor: "white",
+    borderRadius: 12.5,
+    borderWidth: 1,
+  },
+  text: {
+    marginTop: 92,
+    marginBottom: 32,
+    textAlign: "center",
+    fontFamily: "Roboto-Medium",
+    fontSize: 30,
+    lineHeight: 35.16,
+    color: "#212121",
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+});
