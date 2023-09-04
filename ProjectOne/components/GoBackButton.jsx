d2e19221b1c4f0c9bb670f7cd4332eef3303004b
pttkachuk <petrotkachuk6@gmail.com>
2023-09-04 10:39:32 +0200

    create GoBackButton component

---
+++ ProjectOne/components/GoBackButton.jsx
@@ -0,0 +1,20 @@
+import { View, TouchableOpacity } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+const GoBackButton = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={{ marginLeft: 16 }}>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => navigation.navigate("Публікації")}
+      >
+        <Ionicons name="arrow-back-sharp" size={24} color="#212121" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default GoBackButton;
