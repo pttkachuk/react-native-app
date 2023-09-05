86eec82ae437fa5ec73478efd19fd0a2f695452b
pttkachuk <petrotkachuk6@gmail.com>
2023-09-05 10:13:47 +0200

    create firebase config file

---
+++ ProjectOne/firebase/config.js
@@ -0,0 +1,24 @@
+// Для роботи із firebase обовʼязково треба ініціалізувати проект
+import { initializeApp } from "firebase/app";
+// Функція для підключення авторизації в проект
+import { getAuth } from "firebase/auth";
+// Функція для підключення бази даних у проект
+import { getFirestore } from "firebase/firestore";
+// Функція для підключення сховища файлів в проект
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyPWswXsxuhnTjLKWGll6WB4S5hcGKzko",
+    authDomain: "reactnativeprojectone-abcc7.firebaseapp.com",
+    projectId: "reactnativeprojectone-abcc7",
+    databaseURL: "https://reactnativeprojectone-abcc7-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "reactnativeprojectone-abcc7.appspot.com",
+    messagingSenderId: "425014447323",
+    appId: "1:425014447323:web:d2dc1065b1119be506d0fe",
+    measurementId: "G-3DKEBB2TF7",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
