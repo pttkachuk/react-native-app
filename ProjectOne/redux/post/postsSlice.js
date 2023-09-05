f0a03555d4e7c31ff76453b173157f842a12b489
pttkachuk <petrotkachuk6@gmail.com>
2023-09-05 16:29:01 +0200

    create postsSlice and postsSelectors, add base datas

---
+++ ProjectOne/redux/post/postsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    posts: [],
+};
+
+export const postsSlice = createSlice({
+    name: "posts",
+    initialState,
+    reducers: {
+        addPost(state, action) {
+            state.posts = action.payload;
+        },
+    },
+});
+
+export const { addPost } = postsSlice.actions;
+export const postsReducer = postsSlice.reducer;
