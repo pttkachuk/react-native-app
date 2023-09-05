f22d03b8667c5c53ec3f96bf6af64db5334e16e0
pttkachuk <petrotkachuk6@gmail.com>
2023-09-05 16:28:48 +0200

    create authSlice and authSelectors, add base datas

---
+++ ProjectOne/redux/auth/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: { email: "", password: "" },
+    isLoggedIn: false,
+};
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        createUser(state, { payload }) {
+            state.user.email = payload.email;
+            state.user.password = payload.password;
+        },
+        logIn(state, { payload }) {
+            state.user = payload;
+            state.isLoggedIn = true;
+        },
+        logOut(state) {
+            state.user = { email: "", password: "" };
+            state.isLoggedIn = false;
+        },
+    },
+});
+
+export const { createUser, logIn, logOut } = authSlice.actions;
+export const authReducer = authSlice.reducer;
