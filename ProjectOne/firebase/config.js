5c05b0d3659dbf36de98227df41d7652603b358d
pttkachuk <petrotkachuk6@gmail.com>
2023-09-07 09:33:23 +0200

    Update config.js

--- ProjectOne/firebase/config.js
+++ ProjectOne/firebase/config.js
@@ -1,11 +1,12 @@
 // Для роботи із firebase обовʼязково треба ініціалізувати проект
 import { initializeApp } from "firebase/app";
 // Функція для підключення авторизації в проект
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 // Функція для підключення бази даних у проект
 import { getFirestore } from "firebase/firestore";
 // Функція для підключення сховища файлів в проект
 import { getStorage } from "firebase/storage";
+import { ReactNativeAsyncStorage } from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDyPWswXsxuhnTjLKWGll6WB4S5hcGKzko",
@@ -19,6 +20,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+//export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const db = getFirestore(app);
 export const storage = getStorage(app);
